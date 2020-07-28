27c7443565d8680645b5aa6fb2c324dfce7e94d1
Lizbeth <ljgar24@gmail.com>
2020-07-27 17:18:30 -0700

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Apple on 7/27/20.
+//  Copyright © 2020 Liz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
